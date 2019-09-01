1ddf61e5c918929272833d00554afcd1cde6f926
Tyler Ouellette <47769996+Tyler-Ouellette@users.noreply.github.com>
2019-09-01 15:14:57 -0400

    added easytimer library, created recording array of key value pairs

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 window.addEventListener("load", init);
+const recording = [];
+
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
@@ -7,16 +9,65 @@ function removeTransition(e) {
 
 function playSound(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-    const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    const keycode = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
 
-    key.classList.add('playing');
+    const timedNote = [];
+    const key = e.key;
+    const time = document.querySelector('.values').innerText;
+    timedNote.push(key, time);
+    console.log(timedNote);
+
+
+    keycode.classList.add('playing');
     audio.currentTime = 0;
     audio.play();
+
+    recording.push(timedNote);
+    console.log(recording);
+
+
 }
 
 function init() {
     const keys = Array.from(document.querySelectorAll('.key'));
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+    keys.forEach(key => key.addEventListener('keyup', removeTransition));
     window.addEventListener('keydown', playSound);
+
+    var timer = new easytimer.Timer();
+
+    $('#chronoExample .startButton').click(function () {
+        timer.start({
+            precision: 'secondTenths'
+        });
+    });
+    $('#chronoExample .pauseButton').click(function () {
+        timer.pause();
+    });
+    $('#chronoExample .stopButton').click(function () {
+        timer.stop();
+    });
+    $('#chronoExample .resetButton').click(function () {
+        timer.reset();
+        timer.stop();
+    });
+    timer.addEventListener('secondsUpdated', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
+    });
+    timer.addEventListener('secondTenthsUpdated', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
+    });
+    timer.addEventListener('started', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
+    });
+    timer.addEventListener('reset', function (e) {
+        $('#chronoExample .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
+        recording.splice(0, recording.length);
+        console.log(recording);
+    });
+
+    // console.log(recording);
 }
+
+// Will have to track Key Down and Key Up. Key down time and Key up of that same key time (Piano)s
