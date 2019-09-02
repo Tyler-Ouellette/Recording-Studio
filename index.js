37691ebc3010b9c7940d2ade3ab8868be1c0142e
Tyler Ouellette <47769996+Tyler-Ouellette@users.noreply.github.com>
2019-09-01 23:47:12 -0400

    implementing  playback feature to console log recording array

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 window.addEventListener("load", init);
 const recording = [];
+const timeOfRelease = [];
+
+// let lastKeyTime = Date.now(); To be used one day If piano is ever implemented. Nedd to track key downs and up
+
 
 
 function removeTransition(e) {
     if (e.propertyName !== 'transform') return;
-    e.target.classList.remove('playing');
+    this.classList.remove('playing');
 }
 
+/**
+ * This function is used to play the sound 
+ */
 function playSound(e) {
+
+    if (e.repeat) return;
+
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const keycode = document.querySelector(`div[data-key="${e.keyCode}"]`);
     if (!audio) return;
@@ -16,43 +26,99 @@ function playSound(e) {
     audio.currentTime = 0;
     audio.play();
 
-    const timedNote = [];
+    // const currentKeyTime = Date.now();
+    // console.log("First = " + currentKeyTime);
+}
+
+function playAndRecord(e) {
+    if (e.repeat) return;
+
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const keycode = document.querySelector(`div[data-key="${e.keyCode}"]`);
+    if (!audio) return;
+
+    keycode.classList.add('playing');
+    audio.currentTime = 0;
+    audio.play();
+
     const key = e.key;
-    const time = document.querySelector('.values').innerText;
+    const startTime = document.querySelector('.values').innerText;
 
-    if (time === "00:00:00" || time === "00:00:00:0" || time === "00:00:00:00") return;
+    // const currentKeyTime = Date.now();
+    // console.log("First = " + currentKeyTime);
+    recordNote(key, startTime);
+}
 
-    timedNote.push(key, time);
-    console.log(timedNote);
+function recordNote(key, startTime) {
+    const timedNote = [];
 
+    if (startTime === "00:00:00" || startTime === "00:00:00:0" || startTime === "00:00:00:00") return;
+    timedNote.push(key, startTime);
     recording.push(timedNote);
     console.log(recording);
-
-
 }
+
+/**
+ * This function will be used if I do a piano studio one day. Need to track the time of release as a piano note can be held.  
+ * Drum Notes aren't really held down so that is not an issue.
+ */
+// function endSound(e) {
+//     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+//     if (!audio) return;
+
+//     lastKeyTime = Date.now();
+//     console.log("Last = " + lastKeyTime);
+
+//     endTime = document.querySelector('.values').innerText;
+//     if (endTime === "00:00:00" || endTime === "00:00:00:0" || endTime === "00:00:00:00") return;
+
+//     const letter = this.childNodes[1].innerText.toLowerCase();
+//     timeOfRelease.push(letter, endTime);
+// }
+
+// function playbackRecording() {
+//     basicTimer.start();
+//     basicTimer.addEventListener('secondsUpdated', function (e) {
+//         $('.PlaybackTime').html(basicTimer.getTimeValues().toString());
+//         console.log(recording);
+//     });
+// }
 
 function init() {
     const keys = Array.from(document.querySelectorAll('.key'));
     keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-    keys.forEach(key => key.addEventListener('keyup', removeTransition));
+    // keys.forEach(key => key.addEventListener('keyup', removeTransition));
     window.addEventListener('keydown', playSound);
+    // window.addEventListener('keyup', endSound);
 
-    var timer = new easytimer.Timer();
+    /**
+     * Recording Area
+     */
+
+    const timer = new easytimer.Timer();
 
     $('.RecordingArea .startButton').click(function () {
+        window.removeEventListener('keydown', playSound);
+        window.addEventListener('keydown', playAndRecord);
         timer.start({
             precision: 'secondTenths'
         });
     });
     $('.RecordingArea .pauseButton').click(function () {
         timer.pause();
+        window.removeEventListener("keydown", playAndRecord);
+        window.addEventListener("keydown", playSound);
     });
-    $('.RecordingArea .stopButton').click(function () {
+    $('.RecordingArea .endButton').click(function () {
         timer.stop();
+        recording.splice(0, recording.length);
+        window.removeEventListener("keydown", playAndRecord);
+        window.addEventListener("keydown", playSound);
     });
     $('.RecordingArea .resetButton').click(function () {
         timer.reset();
         timer.stop();
+
     });
     timer.addEventListener('secondsUpdated', function (e) {
         $('.RecordingArea .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
@@ -66,10 +132,35 @@ function init() {
     timer.addEventListener('reset', function (e) {
         $('.RecordingArea .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
         recording.splice(0, recording.length);
-        console.log(recording);
     });
 
-    // console.log(recording);
-}
 
-// Will have to track Key Down and Key Up. Key down time and Key up of that same key time (Piano)s
+    /**
+     * Playback  Area
+     */
+    const basicTimer = new easytimer.Timer();
+
+    $('.playArea .PausePlayback').click(function () {
+        basicTimer.pause();
+    });
+
+    $('.playArea .RestartPlayback').click(function () {
+        basicTimer.reset();
+        basicTimer.stop();
+    });
+
+    basicTimer.addEventListener('reset', function (e) {
+        $('.RecordingArea .values').html(timer.getTimeValues().toString(['hours', 'minutes', 'seconds', 'secondTenths']));
+    });
+
+    $('.playArea .PlaybackRecording').click(function () {
+        basicTimer.start();
+        basicTimer.addEventListener('secondsUpdated', function (e) {
+            $('.PlaybackTime').html(basicTimer.getTimeValues().toString());
+            console.log(recording);
+        });
+    });
+
+
+
+}
